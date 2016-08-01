d6516cb24057a9b418827543d7d06b06be61d61e
touma26 <ocramout@gmail.com>
2016-08-01 14:58:12 -0400

    Challenge 5

--- elevators.js
+++ elevators.js
@@ -1,7 +1,9 @@
 {
     init: function(elevators, floors) {
-        var e1 = elevators[0]; 
-        var e2 = elevators[1]; 
+        var e1 = elevators[0];
+        var e2 = elevators[1];
+        var e3 = elevators[2];
+        var e4 = elevators[3];
 
         e1.on("idle", function() {
             e1.goToFloor(0);
@@ -9,6 +11,14 @@
 
         e2.on("idle", function() {
             e2.goToFloor(0);
+        });
+
+        e3.on("idle", function() {
+            e3.goToFloor(0);
+        });
+
+        e4.on("idle", function() {
+            e4.goToFloor(0);
         });
 
         e1.on("floor_button_pressed", function(floorNum) {
@@ -19,6 +29,14 @@
             e2.goToFloor(floorNum);
         });
 
+        e3.on("floor_button_pressed", function(floorNum) {
+            e3.goToFloor(floorNum);
+        });
+
+        e4.on("floor_button_pressed", function(floorNum) {
+            e4.goToFloor(floorNum);
+        });        
+
     },
-    update: function(dt, elevators, floors) {}
+        update: function(dt, elevators, floors) {}
 }
